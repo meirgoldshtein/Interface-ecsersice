cb91e6ba88eed0e5e5fb4bc7695f1366c334ba93
meirgoldshtein <m0548491057@gmail.com>
2024-08-06 16:31:07 +0300

    Add project files.

---
+++ Interface ecsersice/IPerson.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_ecsersice
+{
+    internal interface IPerson
+    {
+        string GetFullName();
+        int GetAge();
+    }
+
+    public class Person : IPerson
+    {
+        protected readonly string Role;
+        protected string FirstName { get; set; }
+
+        protected string LastName { get; set; }
+        protected DateTime BirthDate { get; set; }
+
+        public Person(string role, string firstName, string lastName, DateTime birthDate)
+        {
+            Role = role;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        
+        public DateTime GetBirthDate()
+        {
+            return BirthDate;
+        }
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+        public int GetAge()
+        {
+            return CalculatAge();
+        }
+
+        private int CalculatAge()
+        {
+            return DateTime.Now.Year - BirthDate.Year;
+        }
+    }
+
+
+
+    public class Student : Person
+    {
+        public Student(string role, string firstName, string lastName, DateTime birthDate) : base(role, firstName, lastName, birthDate)
+        {
+
+        }
+        public string GetRole()
+        {
+            return GeneratorRole();
+        }
+
+        private string GeneratorRole()
+        {
+            return Role;
+        }
+    }
+
+
+
+    public class Teacher : Person
+    {
+        public Teacher(string role, string firstName, string lastName, DateTime birthDate) : base(role, firstName, lastName, birthDate)
+        {
+
+        }
+        public string GetRole()
+        {
+            return GeneratorRole();
+        }
+
+        private string GeneratorRole()
+        {
+            return Role;
+        }
+
+    }
+
+    public interface IRepository<T>
+    {
+            public void Add(T item);
+            public void Remove(T item);
+            public T FindById(int id);
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class UserRepository : IRepository<User>
+    {
+        List<User> list = new List<User>();
+        public void Add(User item)
+        {
+
+        }
+        public void Remove(User item)
+        {
+
+        }
+        public User FindById(int id)
+        {
+            return new User
+            {
+                Id = id
+            };
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return list;
+        }
+
+    }
+
+
+    public class ProductRepository : IRepository<Product>
+    {
+        List<Product> list = new List<Product>();
+        public void Add(Product item)
+        {
+
+        }
+        public void Remove(Product item)
+        {
+
+        }
+        public Product FindById(int id)
+        {
+            return new Product
+            {
+                Id = id
+            };
+        }
+
+        public List<Product> GetAllUsers()
+        {
+            return list;
+        }
+
+    }
+
+
+    public interface Imoveable
+    {
+        public void MoveForward();
+        public void MoveBackward();
+    }
+
+    public interface IFlyable
+    {
+        public void Flyoff();
+        public void Land();
+    }
+
+    public class FlyingCar : Imoveable, IFlyable
+    {
+        public void MoveForward()
+        {
+        }
+        public void MoveBackward()
+        {
+        }
+        public void Flyoff()
+        {
+        }
+        public void Land()
+        {
+        }
+
+        public void StartEngine()
+        {
+            MoveForward();
+            Flyoff();
+
+        }
+
+        public void StopEngine()
+        {
+            MoveBackward();
+            Land();
+        }
+    }
+}
+
---
+++ Interface ecsersice/Program.cs
@@ -0,0 +1,29 @@
+﻿using Interface_ecsersice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("התוכנית מתחילה לרוץ");
+
+        List<Person> list = new List<Person>();
+
+        list.Add(new Student("Student", "anit", "nakesh", new DateTime(1990, 1, 1)));
+        list.Add(new Teacher("Teacher", "avi", "ron", new DateTime(1995, 1, 1)));
+        foreach (var item in list)
+        {
+            if (item is Student)
+            {
+                Student student = (Student)item;
+                Console.WriteLine(student.GetRole());
+            }
+            else if (item is Teacher)
+            {
+                Teacher teacher = (Teacher)item;
+                Console.WriteLine(teacher.GetRole());
+            }
+
+        }
+    }
+
+}
